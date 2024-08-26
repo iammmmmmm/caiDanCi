60c3c62875f7f08842fa4b1ba8323a81c2192c3d
iammmmmmm <2946649116@qq.com>
2024-08-26 13:28:10 +0800

    fix: 重构一下控制器，修补注释

--- src/main/java/caidanci/HelloController.java
+++ src/main/java/caidanci/HelloController.java
@@ -1,24 +1,3 @@
-/**
- * Copyright (c) 2019 Gluon All rights reserved.
- * <p>
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met: * Redistributions of source code must retain the
- * above copyright notice, this list of conditions and the following disclaimer. * Redistributions
- * in binary form must reproduce the above copyright notice, this list of conditions and the
- * following disclaimer in the documentation and/or other materials provided with the distribution.
- * * Neither the name of Gluon, any associated website, nor the names of its contributors may be
- * used to endorse or promote products derived from this software without specific prior written
- * permission.
- * <p>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GLUON BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package caidanci;
 
 import java.util.ArrayList;
@@ -46,20 +25,20 @@ import org.kordamp.ikonli.javafx.FontIcon;
 
 public class HelloController {
 
+  private final SqlTools sqlTools = new SqlTools();
   /**
    * 主题 0 默认 1 暗黑
    */
-  int themeFlag = 0;
+  private int themeFlag = 0;
   /**
    * 单词长度
    */
-  int worldLength = 5;
-  int inputTime = 0;
-  boolean gameIsStart = false;
-  String answerWorld = "12345";
-  int rowIndex = 0;
-  List<String> worldList = null;
-  SqlTools sqlToos = new SqlTools();
+  private int wordLength = 5;
+  private int inputTime = 0;
+  private boolean gameIsStart = false;
+  private String answerWord = "12345";
+  private int rowIndex = 0;
+  private List<String> wordList = null;
   @FXML
   private Button changeTheme;
   @FXML
@@ -77,9 +56,11 @@ public class HelloController {
   @FXML
   private Label testText;
 
+  /**
+   * 切换主题
+   */
   @FXML
   void changeTheme() {
-//        System.out.println(themeFlag);
     if (themeFlag == 1) {
       Application.setUserAgentStylesheet(
           Objects.requireNonNull(this.getClass().getResource("theme/primer-light.css"))
@@ -95,6 +76,9 @@ public class HelloController {
     }
   }
 
+  /**
+   * 处理输入框的键盘释放事件
+   */
   @FXML
   void inputFiledKeyRelease(KeyEvent event) {
     if (event.getCode() == KeyCode.ENTER) {
@@ -102,64 +86,51 @@ public class HelloController {
     }
   }
 
+  /**
+   * 处理输入按钮点击事件
+   */
   @FXML
   void inputButtonClicked() {
-    var inputText = "";
-    if (inputTextFiled.getText() != null && !inputTextFiled.getText().isEmpty()) {
-      inputText = inputTextFiled.getText();
-      if (inputText.length() == worldLength) {
-        if (checkWorld(inputText)) {
+    String inputText = inputTextFiled.getText();
+    if (inputText != null && !inputText.isEmpty()) {
+      if (inputText.length() == wordLength) {
+        if (checkWord(inputText)) {
           if (inputTime == 0) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("不，你失败了！");
-            alert.setHeaderText("你耗尽了机会！");
-            alert.setContentText("答案：" + answerWorld + " :\n" + sqlToos.getWordInfo(answerWorld));
-            alert.showAndWait();
+            showAlert(Alert.AlertType.INFORMATION, "不，你失败了！", "你耗尽了机会！",
+                "答案：" + answerWord + " :\n" + sqlTools.getWordInfo(answerWord));
             startGame();
           } else {
             inputTime--;
-            if (worldList.contains(inputText)) {
-              Alert alert = new Alert(Alert.AlertType.WARNING);
-              alert.setTitle("不");
-              alert.setHeaderText("你已经猜过这个单词了！");
-              alert.setContentText(inputText + " :\n" + sqlToos.getWordInfo(inputText));
-              alert.showAndWait();
+            if (wordList.contains(inputText)) {
+              showAlert(Alert.AlertType.WARNING, "不", "你已经猜过这个单词了！",
+                  inputText + " :\n" + sqlTools.getWordInfo(inputText));
               inputTextFiled.clear();
             } else {
-              worldList.add(inputText);
+              wordList.add(inputText);
               refresh(inputText);
-              if (inputText.equals(answerWorld)) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("胜利！");
-                alert.setHeaderText("恭喜你，成功猜出单词！");
-                alert.setContentText(answerWorld + " :\n" + sqlToos.getWordInfo(answerWorld));
-                alert.showAndWait();
+              if (inputText.equals(answerWord)) {
+                showAlert(Alert.AlertType.INFORMATION, "胜利！", "恭喜你，成功猜出单词！",
+                    answerWord + " :\n" + sqlTools.getWordInfo(answerWord));
                 startGame();
               }
             }
           }
         } else {
-          Alert alert = new Alert(Alert.AlertType.ERROR);
-          alert.setTitle("错误！");
-          alert.setHeaderText("输入的单词错误！");
-          alert.setContentText("应该输入一个正确的单词");
-          alert.showAndWait();
+          showAlert(Alert.AlertType.ERROR, "错误！", "输入的单词错误！", "应该输入一个正确的单词");
         }
       } else {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("错误！");
-        alert.setHeaderText("输入格式错误！");
-        alert.setContentText("你应该输入一个单词，且该单词长度为" + worldLength + "！");
-        alert.showAndWait();
+        showAlert(Alert.AlertType.WARNING, "错误！", "输入格式错误！",
+            "你应该输入一个单词，且该单词长度为" + wordLength + "！");
       }
     }
     inputTextFiled.clear();
   }
 
+  /**
+   * 刷新显示
+   */
   private void refresh(String inputText) {
-
     int charSize = 40; // 每个字符的大小
-
     outputGrid.setAlignment(Pos.CENTER); // 居中对齐
 
     // 将字符串分割成字符数组，并在每个格子中添加一个标签
@@ -183,13 +154,16 @@ public class HelloController {
    *
    * @return 是否合法
    */
-  private boolean checkWorld(String word) {
-    return sqlToos.checkWord(word);
+  private boolean checkWord(String word) {
+    return sqlTools.checkWord(word);
   }
 
+  /**
+   * 初始化方法
+   */
   @FXML
   void initialize() {
-    var a = Font.loadFont(
+    Font a = Font.loadFont(
         Objects.requireNonNull(this.getClass()
             .getResourceAsStream("fonts/HeavyData/HeavyDataNerdFontPropo-Regular.ttf")), 20);
     System.out.println(a.getFamily());
@@ -203,60 +177,65 @@ public class HelloController {
     gameIsStart = false;
 
     SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(
-        3, 10, worldLength);
+        3, 10, wordLength);
     levelChose.setValueFactory(valueFactory);
     levelChose.valueProperty().addListener((observable, oldValue, newValue) -> {
-      worldLength = newValue;
+      wordLength = newValue;
       inputTimeUpdate();
     });
     inputTimeUpdate();
-
-
   }
 
+  /**
+   * 更新输入次数
+   */
   private void inputTimeUpdate() {
-    inputTime = worldLength - rowIndex + 1;
-    inputButton.setText("输入" + " " + (inputTime));
+    inputTime = wordLength - rowIndex + 1;
+    inputButton.setText("输入 " + inputTime);
   }
 
-  // 获取随机颜色
+  /**
+   * 获取颜色
+   */
   private Color getColor(String letter, int index) {
-    Color color;
-    if (answerWorld.contains(letter)) {
-      if (answerWorld.indexOf(letter, index - 1) == index) {
-        color = Color.GREEN;
+    if (answerWord.contains(letter)) {
+      if (answerWord.indexOf(letter, index - 1) == index) {
+        return Color.GREEN;
       } else {
-        color = Color.YELLOW;
+        return Color.YELLOW;
       }
     } else {
-      color = Color.GRAY;
+      return Color.GRAY;
     }
-    return color;
   }
 
+  /**
+   * 处理输入按钮触摸事件
+   */
   @FXML
   void inputButtonTouched() {
     inputButtonClicked();
   }
 
+  /**
+   * 处理信息按钮点击事件
+   */
   @FXML
   void infoButtonClicked() {
-    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle("游戏说明");
-    alert.setHeaderText("游戏说明");
-    alert.setContentText("""
+    showAlert(Alert.AlertType.INFORMATION, "游戏说明", "游戏说明", """
         答案为指定长度单词，输入对应长度单词即可;
         绿色代表此单词中有此字母且位置正确;
         黄色代表此单词中有此字母，但该字母所处位置不对
         灰色代表此单词中没有此字母
         一共有单词长度+1次机会，机会用尽前猜中即为胜利
         """);
-    alert.showAndWait();
   }
 
+  /**
+   * 开始游戏
+   */
   @FXML
   void startGame() {
-
     if (gameIsStart) {
       startGame.setText("开始游戏");
       levelChose.setDisable(false);
@@ -271,19 +250,35 @@ public class HelloController {
       inputButton.setDisable(false);
       inputTextFiled.setDisable(false);
       gameIsStart = true;
-      worldList = new ArrayList<>();
+      wordList = new ArrayList<>();
     }
     inputTimeUpdate();
   }
 
+  /**
+   * 重置游戏
+   */
   private void reset() {
     answerUpdate();
     rowIndex = 0;
     outputGrid.getChildren().clear();
   }
 
+  /**
+   * 更新答案
+   */
   private void answerUpdate() {
-    answerWorld = sqlToos.getAnswer(worldLength);
+    answerWord = sqlTools.getAnswer(wordLength);
   }
 
+  /**
+   * 显示提示框
+   */
+  private void showAlert(Alert.AlertType alertType, String title, String header, String content) {
+    Alert alert = new Alert(alertType);
+    alert.setTitle(title);
+    alert.setHeaderText(header);
+    alert.setContentText(content);
+    alert.showAndWait();
+  }
 }
